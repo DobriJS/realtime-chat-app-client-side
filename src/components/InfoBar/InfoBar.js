a446b8a92f485c33a4b3ad0ece8d275596b42df1
DobriJS <53184157+DobriJS@users.noreply.github.com>
2020-11-01 16:32:01 +0200

    Fix InfoBar.css

--- src/components/InfoBar/InfoBar.js
+++ src/components/InfoBar/InfoBar.js
@@ -1,8 +1,9 @@
 import React from "react";
-import "./InfoBar.css";
 
 import closeIcon from "../../Icons/closeIcon.png";
 import onlineIcon from "../../Icons/onlineIcon.png";
+
+import "./InfoBar.css";
 
 const InfoBar = ({ room }) => (
   <div className="infoBar">
