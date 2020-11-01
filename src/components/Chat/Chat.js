ed8570640b69252e2d93d5189e803d491503f004
DobriJS <53184157+DobriJS@users.noreply.github.com>
2020-11-01 15:04:12 +0200

    Add Messages and Message components

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -5,6 +5,7 @@ import "./Chat.css";
 
 import InfoBar from "../InfoBar/InfoBar";
 import Input from "../Input/Input";
+import Messages from "../Messages/Messages";
 
 let socket;
 
@@ -50,7 +51,7 @@ const Chat = ({ location }) => {
     <div className="outerContainer">
       <div className="container">
         <InfoBar room={room} />
-
+        <Messages messages={messages} name={name} />
         <Input
           message={message}
           setMessage={setMessage}
--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -2,14 +2,14 @@ import React from "react";
 
 import "./Input.css";
 
-const Input = ({ message, setMessage, sendMessage }) => (
+const Input = ({ setMessage, sendMessage, message }) => (
   <form className="form">
     <input
       className="input"
       type="text"
       placeholder="Type a message..."
       value={message}
-      onChange={(event) => setMessage(event.target.value)}
+      onChange={({ target: { value } }) => setMessage(value)}
       onKeyPress={(event) =>
         event.key === "Enter" ? sendMessage(event) : null
       }
